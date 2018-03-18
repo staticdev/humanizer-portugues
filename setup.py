721b55f5363ce8047dad710bd8421fc10d504485
staticdev <thiagocavila@gmail.com>
2018-03-18 16:39:14 -0300

    #1 removendo distutils

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 import sys, os
 import io
 
