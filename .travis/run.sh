bb935b245d231f6702b4513a5b236208a783e499
Thiago C. D'Ávila <thiagocavila@ufmg.br>
2018-03-19 12:53:46 -0300

    Create run.sh

---
+++ .travis/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    PYENV_ROOT="$HOME/.pyenv-humanizer-portugues"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    hash -r
+    eval "$(pyenv init -)"
+fi
+REQUIRE_SPEEDUPS=1 python setup.py build_ext -i
+python -m compileall -f .
+python setup.py test
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    python setup.py bdist_wheel
+fi
+
+if [[ $BUILD_SDIST == 'true' ]]; then
+    python setup.py sdist
+fi
