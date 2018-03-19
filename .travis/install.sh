d1c02c0c9a613c079550a28028ffa124bb15fe31
Thiago C. D'Ávila <thiagocavila@ufmg.br>
2018-03-19 12:51:30 -0300

    Create install.sh

---
+++ .travis/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    if [ ! -e "$HOME/.pyenv-humanizer-portugues/.git" ]; then
+      if [ -e "$HOME/.pyenv-humanizer-portugues" ]; then
+        rm -rf ~/.pyenv-simplejson
+      fi
+      git clone https://github.com/yyuu/pyenv.git ~/.pyenv-humanizer-portugues
+    else
+      (cd ~/.pyenv-humanizer-portugues; git pull)
+    fi
+    PYENV_ROOT="$HOME/.pyenv-humanizer-portugues"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    hash -r
+    eval "$(pyenv init -)"
+    hash -r
+    pyenv install --list
+    pyenv install -s $PYENV_VERSION
+    pip install wheel
+fi
