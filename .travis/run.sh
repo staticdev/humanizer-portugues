95b6e76d47f5cf4a8c9ac812cd02a2f5c39c60a3
staticdev <thiagocavila@gmail.com>
2018-03-20 19:17:25 -0300

    #4 travis/run.sh

---
+++ .travis/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+REQUIRE_SPEEDUPS=1 python setup.py build_ext -i
+python -m compileall -f .
+coverage run --omit="*/tests/*" -m unittest discover
+
+if [[ $BUILD_SDIST == 'true' ]]; then
+    python setup.py sdist
+fi
