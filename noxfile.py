ee97aa07734e10bd7870668ef7e01b5d53889c63
staticdev <thiagocavila@gmail.com>
2020-06-24 19:11:14 -0300

    Typeguard enforcement

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ from nox.sessions import Session
 
 package = "humanizer_portugues"
 python_versions = ["3.8", "3.7"]
-nox.options.sessions = "pre-commit", "safety", "mypy", "tests"
+nox.options.sessions = "pre-commit", "safety", "mypy", "tests", "typeguard"
 
 
 class Poetry:
